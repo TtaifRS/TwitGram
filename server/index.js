5c604b00089b5b9d7edff931c00646017fe9350b
Taif Rahaman <taif.rahaman73@gmail.com>
2021-07-09 17:38:27 +0600

    add get user and follow user

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
+    useFindAndModify: false,
   })
   .then(() => {
     console.log('Connected to mongo');
--- server/models/User.js
+++ server/models/User.js
@@ -29,7 +29,7 @@ const UserSchema = new mongoose.Schema(
       type: String,
       default: '',
     },
-    follwers: {
+    followers: {
       type: Array,
       default: [],
     },
