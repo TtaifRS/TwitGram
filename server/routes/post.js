c537988f2daa36b7c208565a829043049d3e791d
Taif Rahaman <taif.rahaman73@gmail.com>
2021-07-09 18:34:52 +0600

    update and delete post

--- server/routes/post.js
+++ server/routes/post.js
@@ -12,4 +12,36 @@ router.post('/', async (req, res) => {
   }
 });
 
+//update post
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json('post has been updated');
+    } else {
+      res.status(403).json('you can update only your post');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
+});
+
+//delete post
+router.delete('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json('post has been deleted');
+    } else {
+      res.status(403).json('you can update only your post');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
+});
+
 module.exports = router;
